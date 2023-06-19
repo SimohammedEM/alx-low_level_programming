2e728cbdff2e8dbb7e6defaa662e352a8175ff67
SimohammedEM <elmorslimohammedem@gmail.com>
2023-06-19 14:11:15 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+  *main -entry point
+  *Description: 'print alphbet lowercase'
+  *Return: Always 0
+  */
+int main(void)
+{
+	int n = 65;
+while (n <= 90)
+{
+	putchar(n);
+	n++;
+}
+	return (0);
+
+}
