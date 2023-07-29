15e891d040b34d0f400d4b500414cc51af0a626b
SimohammedEM <elmorslimohammedem@gmail.com>
2023-07-29 11:26:41 -0700

    a

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,14 +17,14 @@ struct dog
 };
 
 /**
- * dog_type - typedef for the struct dog
+ * dog_t - typedef for the struct dog
  */
-typedef struct dog dog_type;
+typedef struct dog dog_t;
 
-void init_dog(struct dog *chien, char *name, float age, char *owner);
-void print_dog(struct dog *chien);
-dog_type *new_dog(char *name, float age, char *owner);
-void free_dog(dog_type *chien);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 
