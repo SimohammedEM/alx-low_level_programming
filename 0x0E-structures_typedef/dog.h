9aab5b6da15edb4d20f439e6ecb56edaf95369fd
SimohammedEM <elmorslimohammedem@gmail.com>
2023-07-29 07:34:50 -0700

    rt

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,32 @@
+#ifndef chien
+#define chien
+
+/**
+ * struct dog - des informations basique sur un chien
+ * @owner: le propriétaire du chien
+ * @name: le nom du chien
+ * @age: l'age du chien
+ *
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+
+};
+
+/**
+ * Type_Dog - typedefinition de la structure struct dog
+ */
+typedef struct dog Type_Dog;
+
+void initial_chien(struct dog *chien, char *name, float age, char *owner);
+void printing_chien(struct dog *chien);
+Type_Dog *Nouveau_chien(char *name, float age, char *owner);
+void un_chien(Type_Dog *chien);
+/**char *_strcpy(char *dest, char *src);
+int _strlen(char *s);*/
+
+#endif
+
