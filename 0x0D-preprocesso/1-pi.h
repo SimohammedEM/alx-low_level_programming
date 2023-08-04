6b2287eee225246166945fbb86ac1d0c52f37db3
SimohammedEM <elmorslimohammedem@gmail.com>
2023-08-04 04:37:22 -0700

    re

--- 0x0D-preprocesso/1-pi.h
+++
@@ -1,7 +0,0 @@
-#ifndef pi_value
-#define pi_value
-
-#define PI 3.14159265359
-
-#endif
-
--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,18 +1,17 @@
 #include "lists.h"
 
 /**
- * print_listint - print linked list
- * @h: ointer to first node
+ * listint_len - prints linked lists
+ * @h: pointer to first node
  *
  * Return: size of list
  */
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	size_t nbr = 0;
 
 	while (h)
 	{
-		printf("%d\n", h->n);
 		h = h->next;
 		nbr++;
 	}
